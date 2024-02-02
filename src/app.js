990cba11ac3f047c270086d412c43d2c609b472a
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-02-02 20:32:38 +0000

    Carta renderizada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //DOM variables
+  const left = document.querySelector("#myCard").childNodes[1];
+  const number = document.querySelector("#myCard").childNodes[3];
+  const right = document.querySelector("#myCard").childNodes[5];
 };
